ff5927d1b3981ff5384ae4215c474f46c8d028f3
vincenzodivita <divitavincenzo@icloud.com>
2023-07-20 22:24:17 +0200

    Add component

---
+++ src/components/Details.vue
@@ -0,0 +1,47 @@
+<script>
+import axios from "axios";
+
+export default {
+  name: "Details",
+  props: {
+		id: String,
+	},
+  data() {
+    return {
+        arrDetails: [],
+    };
+  },
+  methods: {
+    getDetails() {
+      axios
+        .get("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=", {
+          params: {
+            i: this.id,
+          },
+        }) // api link
+        .then((response) => {
+          this.arrDetails = response.data.drinks;
+          console.log(arrDetails)
+        });
+    },
+  },
+  watch: {
+    currentPage() {
+      this.getDetails();
+    },
+  },
+  created() {
+    this.getDetails();
+  },
+};
+</script>
+
+<template>
+
+    <span> {{ id.strDrink }}</span>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
