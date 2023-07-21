da79b3f84bbddae17832a6ab0a56346a3d96f421
Pierluca-Strianese <pierluca.strianese@gmail.com>
2023-07-21 14:51:25 +0200

    fix StyleFooter

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,7 +3,7 @@ export default {};
 </script>
 
 <template>
-  <footer class="py-1 bg-dark text-light">
+  <footer class="py-3 bg-dark text-light">
     <div class="info p-1">
       <div class="description p-2">
         <div class="py-3 border-bottom">
@@ -13,21 +13,21 @@ export default {};
         </div>
         <h6 class="pt-2"> LEARN MORE ABOUT US </h6>
         <div class="social">
-          <font-awesome-icon icon="fa-brands fa-square-facebook" class="icon_social facebook" />
-          <font-awesome-icon icon="fa-brands fa-square-twitter" class="icon_social twitter" />
-          <font-awesome-icon icon="fa-brands fa-square-youtube" class="icon_social youtube" />
-          <font-awesome-icon icon="fa-brands fa-square-instagram" class="icon_social instagram" />
+          <font-awesome-icon icon="fa-brands fa-square-facebook" class="icon_social facebook ps-2" />
+          <font-awesome-icon icon="fa-brands fa-square-twitter" class="icon_social twitter border-start ps-2" />
+          <font-awesome-icon icon="fa-brands fa-square-youtube" class="icon_social youtube border-start ps-2" />
+          <font-awesome-icon icon="fa-brands fa-square-instagram" class="icon_social instagram border-start ps-2" />
         </div>
       </div>
-      <div class="contact_info p-2 text-center">
+      <div class="contact_info p-2 text-end border-start">
         <h5 class="pt-3">Contact infomation</h5>
 
-        <ul class="border-top">
+        <ul>
           <li class="pt-1">
-            <font-awesome-icon icon="fa-solid fa-location-dot" class="icon_contact" /> street
+            via inventata 84 <font-awesome-icon icon="fa-solid fa-location-dot" class="ms-2 icon_contact" />
           </li>
           <li>
-            <font-awesome-icon icon="fa-solid fa-phone-flip" class="icon_contact" /> +02 3432 45325432 532
+            +02 3432 4532 532 <font-awesome-icon icon="fa-solid fa-phone" class="ms-2 icon_contact" />
           </li>
         </ul>
       </div>
@@ -38,4 +38,51 @@ export default {};
     Daniele Savoia, Gary Smith Alvarez Venturo
   </div>
 </template>
-<style></style>
+<style lang="scss" scoped>
+@use "../style/general.scss" as *;
+
+
+footer {
+  display: flex;
+
+  .info {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+
+    .description {
+      font-size: .8rem;
+      width: 40%;
+    }
+
+    .contact_info {
+      font-size: .9rem;
+      width: 30%;
+
+      ul {
+        li {
+
+          margin-top: 1rem;
+
+          .icon_contact {
+            color: white;
+            margin-right: 1rem;
+          }
+        }
+      }
+    }
+
+    .icon_social {
+      font-size: 1.8rem;
+      padding-right: .5rem;
+    }
+  }
+}
+
+.copyright {
+  background-color: $background_color2;
+  font-size: .7rem;
+  padding-left: 48px;
+}
+</style>
+
--- src/main.js
+++ src/main.js
@@ -15,14 +15,15 @@ import { faSquareTwitter } from '@fortawesome/free-brands-svg-icons'
 import { faSquareInstagram } from '@fortawesome/free-brands-svg-icons'
 import { faSquareYoutube } from '@fortawesome/free-brands-svg-icons'
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
-import { faPhoneFlip } from '@fortawesome/free-solid-svg-icons'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
 import { faCaretRight } from '@fortawesome/free-solid-svg-icons'
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
-library.add(faSquareFacebook, faSquareTwitter, faSquareInstagram, faSquareYoutube, faLocationDot, faPhoneFlip, faCaretRight, faAngleDown)
+library.add(faSquareFacebook, faSquareTwitter, faSquareInstagram, faSquareYoutube, faLocationDot, faPhone, faCaretRight, faAngleDown)
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)
     .use(router)
     .mount("#app");
+
