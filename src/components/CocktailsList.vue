b333dd15bffedb98930d0d1569b7d40e39b3e81b
vincenzodivita <divitavincenzo@icloud.com>
2023-07-21 11:59:08 +0200

    Add h1 title

--- src/components/CocktailsList.vue
+++ src/components/CocktailsList.vue
@@ -40,11 +40,15 @@ export default {
   },
   created() {
     this.getCocktails();
+
   },
 };
+
 </script>
 
+
 <template>
+  <h1 class="pt-5 text-center">Cocktails List</h1>
   <div class=" big_container mb-5 border-top">
     <CocktailsCard v-for="cocktail in arrCocktails" :key="cocktail.id" :cocktail="cocktail" :nPages="this.currentPage" />
   </div>
@@ -67,6 +71,32 @@ export default {
       </li>
     </ul>
   </nav>
+
+  <!-- Button trigger modal -->
+<!-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+  Launch demo modal
+</button> -->
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <style lang="scss" scoped>
