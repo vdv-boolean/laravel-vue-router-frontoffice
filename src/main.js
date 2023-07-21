ed17a9a5b0975acff95afbd8e051923899368ac1
Pierluca-Strianese <pierluca.strianese@gmail.com>
2023-07-21 12:44:56 +0200

    Style Footer

--- src/main.js
+++ src/main.js
@@ -2,4 +2,27 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import * as bootstrap from "bootstrap";
 import { router } from "./router";
-createApp(App).use(router).mount("#app");
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faSquareFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faSquareTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faSquareInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faSquareYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { faPhoneFlip } from '@fortawesome/free-solid-svg-icons'
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faSquareFacebook, faSquareTwitter, faSquareInstagram, faSquareYoutube, faLocationDot, faPhoneFlip, faCaretRight, faAngleDown)
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .use(router)
+    .mount("#app");
