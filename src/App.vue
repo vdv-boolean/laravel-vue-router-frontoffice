d6035f7f48e8bd144f7e5aa960c0c73beff14858
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-07-21 09:57:49 +0200

    link footer

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <script>
-import AppNav from './components/AppNav.vue';
-import CocktailsList from './components/CocktailsList.vue';
+import AppNav from "./components/AppNav.vue";
+import CocktailsList from "./components/CocktailsList.vue";
+import AppFooter from "./components/AppFooter.vue";
 export default {
-    components: {
-        AppNav,
-        CocktailsList,
-    }
-}
-
+  components: {
+    AppNav,
+    CocktailsList,
+    AppFooter,
+  },
+};
 </script>
 
 <template>
-    <AppNav />
-    <router-view></router-view>
+  <AppNav />
+  <router-view></router-view>
+  <AppFooter />
 </template>
 
-<style lang=scss>
-@import 'bootstrap/scss/bootstrap';
+<style lang="scss">
+@import "bootstrap/scss/bootstrap";
 </style>
