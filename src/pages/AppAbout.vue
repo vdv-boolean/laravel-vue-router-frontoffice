dcacc8af215d1417ab538c9de0bb2d33183a9283
Pierluca-Strianese <pierluca.strianese@gmail.com>
2023-07-21 13:51:09 +0200

    Style AboutUs

--- src/pages/AppAbout.vue
+++ src/pages/AppAbout.vue
@@ -47,35 +47,64 @@ export default {};
         </p>
       </div>
     </div>
-    <div class="d-flex my-3">
-      <img src="src/img/dj_1.jpeg" alt="" class="dimension_30 pb-3">
-      <section class="mx-3">
-        <h3 class="mt-4">Spettacoli e Intrattenimento</h3>
-        <p class="lead fs-6 px-2">
-          Presso il Booltails Bar, l'intrattenimento è una componente fondamentale
-          dell'esperienza. Offriamo regolarmente spettacoli dal vivo, come
-          performance musicali di artisti locali, serate a tema e DJ set che vi
-          faranno ballare fino a tarda notte.
-        </p>
+
+    <section class="d-flex p-5">
+      <section class="pt-5">
+        <div class="d-flex py-3">
+          <img src="src/img/dj_1.jpeg" alt="" class="dimension_1_1 pb-3">
+          <section class="mx-3">
+            <h3 class="mt-1">Spettacoli e Intrattenimento</h3>
+            <p class="lead fs-6 px-2">
+              Presso il Booltails Bar, l'intrattenimento è una componente fondamentale
+              dell'esperienza. Offriamo regolarmente spettacoli dal vivo, come
+              performance musicali di artisti locali, serate a tema e DJ set che vi
+              faranno ballare fino a tarda notte.
+            </p>
+          </section>
+        </div>
+
+        <div class="d-flex py-3">
+          <img src="src/img/client_2.jpeg" alt="" class="dimension_1_1 pb-3">
+          <section class="mx-3">
+            <h3 class="mt-1">Vieni quando vuoi</h3>
+            <p class="lead fs-6 px-2">
+              Il nostro Booltails Bar è aperto dalle 8am alle 1am, escudendo orari speciali legati ad eventi.
+              Vieni da noi per gustare un ottima coalzione, a fare un aperitvo con gli amici, a gustare un pranzo ricco di
+              sapori locali o una cena ricca preparata
+              dai migliori chef.
+            </p>
+          </section>
+        </div>
+
+        <div class="d-flex py-3">
+          <img src="src/img/client_1.png" alt="" class="dimension_1_1 pb-3">
+          <section class="mx-3">
+            <h3 class="mt-1">Servizio Clienti</h3>
+            <p class="lead fs-6 px-2">
+              Il nostro staff dedicato è sempre pronto ad assistervi e a consigliarvi
+              nelle vostre scelte, garantendo un servizio cortese e professionale. Siamo
+              aperti a soddisfare eventuali richieste particolari per rendere la vostra
+              visita al Booltails Bar unica e memorabile.
+            </p>
+          </section>
+        </div>
       </section>
-    </div>
 
-    <h3 class="mt-4">Servizio Clienti</h3>
-    <p class="lead fs-6 px-2">
-      Il nostro staff dedicato è sempre pronto ad assistervi e a consigliarvi
-      nelle vostre scelte, garantendo un servizio cortese e professionale. Siamo
-      aperti a soddisfare eventuali richieste particolari per rendere la vostra
-      visita al Booltails Bar unica e memorabile.
-    </p>
+      <div class="py-3">
+        <img src="src/img/wedding_1.jpeg" alt="" class="dimension_100 pb-3">
+        <section class="mx-3">
+          <h3 class="mt-4">Eventi Speciali</h3>
+          <p class="lead fs-6 px-2">
+            Oltre alle serate di intrattenimento regolari, il nostro Booltails Bar
+            organizza anche eventi speciali per celebrare occasioni uniche come feste
+            di compleanno, anniversari o altre festività. Vi invitiamo a contattarci
+            per prenotare il vostro evento speciale e far sì che sia davvero
+            indimenticabile.
+          </p>
+        </section>
+      </div>
+    </section>
 
-    <h3 class="mt-4">Eventi Speciali</h3>
-    <p class="lead fs-6 px-2">
-      Oltre alle serate di intrattenimento regolari, il nostro Booltails Bar
-      organizza anche eventi speciali per celebrare occasioni uniche come feste
-      di compleanno, anniversari o altre festività. Vi invitiamo a contattarci
-      per prenotare il vostro evento speciale e far sì che sia davvero
-      indimenticabile.
-    </p>
   </div>
 </template>
 
