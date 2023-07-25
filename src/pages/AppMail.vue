3710c643cb7803d116ff22fdb5749f9753fd84b5
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-07-25 15:58:02 +0200

    mod style mail page

--- src/pages/AppMail.vue
+++ src/pages/AppMail.vue
@@ -47,72 +47,80 @@ export default {
 </script>
 
 <template>
-  <h1>contact us</h1>
+  <body>
+    <div class="container">
+      <h1>Contact Us</h1>
 
-  <div
-    v-if="hasError"
-    class="alert alert-danger alert-dismissible fade show"
-    role="alert"
-  >
-    errore nella compilazione!!
-    <button
-      type="button"
-      class="btn-close"
-      data-bs-dismiss="alert"
-      aria-label="Close"
-    ></button>
-  </div>
-
-  <div
-    v-if="showSuccess"
-    class="alert alert-success alert-dismissible fade show"
-    role="alert"
-  >
-    messaggio inviato con successo!!
-    <button
-      type="button"
-      class="btn-close"
-      data-bs-dismiss="alert"
-      aria-label="Close"
-    ></button>
-  </div>
-
-  <form @submit.prevent="sendLead" novalidate>
-    <div class="mb-3">
-      <label for="name" class="form-label">Name</label>
-      <input type="name" class="form-control" id="name" v-model="name" />
-    </div>
-
-    <div class="mb-3">
-      <label for="email" class="form-label">Email address</label>
-      <input type="email" class="form-control" id="email" v-model="email" />
-    </div>
-
-    <div class="mb-3">
-      <label for="message" class="form-label">Message</label>
-      <textarea
-        id="message"
-        class="form-control"
-        rows="5"
-        v-model="message"
-      ></textarea>
-    </div>
-
-    <div class="mb-3 form-check">
-      <input
-        type="checkbox"
-        class="form-check-input"
-        id="newsletter"
-        v-model="newsletter"
-      />
-      <label class="form-check-label" for="newsletter"
-        >subscribe to newsletter</label
+      <div
+        v-if="hasError"
+        class="alert alert-danger alert-dismissible fade show"
+        role="alert"
       >
+        errore nella compilazione!!
+        <button
+          type="button"
+          class="btn-close"
+          data-bs-dismiss="alert"
+          aria-label="Close"
+        ></button>
+      </div>
+
+      <div
+        v-if="showSuccess"
+        class="alert alert-success alert-dismissible fade show"
+        role="alert"
+      >
+        messaggio inviato con successo!!
+        <button
+          type="button"
+          class="btn-close"
+          data-bs-dismiss="alert"
+          aria-label="Close"
+        ></button>
+      </div>
+
+      <form @submit.prevent="sendLead" novalidate>
+        <div class="mb-3">
+          <label for="name" class="form-label">Name</label>
+          <input type="name" class="form-control" id="name" v-model="name" />
+        </div>
+
+        <div class="mb-3">
+          <label for="email" class="form-label">Email address</label>
+          <input type="email" class="form-control" id="email" v-model="email" />
+        </div>
+
+        <div class="mb-3">
+          <label for="message" class="form-label">Message</label>
+          <textarea
+            id="message"
+            class="form-control"
+            rows="5"
+            v-model="message"
+          ></textarea>
+        </div>
+
+        <div class="mb-3 form-check">
+          <input
+            type="checkbox"
+            class="form-check-input"
+            id="newsletter"
+            v-model="newsletter"
+          />
+          <label class="form-check-label" for="newsletter"
+            >subscribe to newsletter</label
+          >
+        </div>
+        <button type="submit" class="btn btn-primary" :disabled="isSending">
+          Submit
+        </button>
+      </form>
     </div>
-    <button type="submit" class="btn btn-primary" :disabled="isSending">
-      Submit
-    </button>
-  </form>
+  </body>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+body {
+  height: calc(100vh - 21.5rem);
+}
+</style>
