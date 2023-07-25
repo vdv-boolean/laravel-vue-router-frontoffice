c7aacabc9a93f0dfab66c6eb199ea1c447e5f8c6
vincenzodivita <divitavincenzo@icloud.com>
2023-07-25 18:39:19 +0200

    Import CocktailList component

---
+++ src/pages/AppCocktail.vue
@@ -0,0 +1,21 @@
+<script>
+
+import CocktailList from '../components/CocktailList.vue';
+
+export default {
+    components: {
+        CocktailList,
+    },
+}
+
+</script>
+
+<template>
+
+<CocktailList />
+
+</template>
+
+<style scoped>
+
+</style>
