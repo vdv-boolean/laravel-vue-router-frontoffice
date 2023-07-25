e5acfd670d351be77387e90664d30a83857b0282
vincenzodivita <divitavincenzo@icloud.com>
2023-07-25 18:45:33 +0200

    Removed everything but CocktailsCard for loop

--- src/components/CocktailsList.vue
+++ src/components/CocktailsList.vue
@@ -61,57 +61,11 @@ export default {
 
 
 <template>
-  <h1 class="pt-5 text-center">Cocktails List</h1>
-  <div class=" big_container mb-5 border-top">
+
     <CocktailsCard v-for="cocktail in getSlicedCocktails" :key="cocktail.id" :cocktail="cocktail"
       :nPages="this.currentPage" />
-  </div>
 
-  <!-- PAGINATOR -->
-  <nav class="nav_bar pb-5">
-    <ul class="pagination">
-      <li class="page-item" :class="{ disabled: currentPage === 1 }">
-        <a class="page-link" href="#" @click="currentPage--">
-          <span aria-hidden="true">&laquo;</span>
-        </a>
-      </li>
-      <li v-for="page in nPages" :key="page" class="page-item" :class="{ active: page === currentPage }">
-        <a class="page-link" href="#" @click="currentPage = page">{{ page }}</a>
-      </li>
-      <li class="page-item" :class="{ disabled: currentPage === nPages }">
-        <a class="page-link" href="#" @click="currentPage++">
-          <span aria-hidden="true">&raquo;</span>
-        </a>
-      </li>
-    </ul>
-  </nav>
-
-  <!-- Button trigger modal -->
-  <!-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
-  Launch demo modal
-</button> -->
-
-  <!-- Modal -->
-  <div class="modal fade " id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
-    aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          ...
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-      </div>
-    </div>
-  </div>
+  
 </template>
 
 <style lang="scss" scoped>
