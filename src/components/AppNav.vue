e757a95600d07347007adafe9eb87a86970be7a2
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-07-25 17:06:10 +0200

    add our team in nav

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -27,26 +27,10 @@ export default {};
               >Contact Us</router-link
             >
           </li>
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              href="#"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
+          <li class="nav-item mx-2">
+            <router-link :to="{ name: 'ourteam' }" class="navbar-brand"
+              >Our Team</router-link
             >
-              Contacts
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Vincenzo Di Vita</a></li>
-              <li><a class="dropdown-item" href="#">Andrea Lorenzi</a></li>
-              <li><a class="dropdown-item" href="#">Pierluca Strianese</a></li>
-              <li><a class="dropdown-item" href="#">Irene Caiazzo</a></li>
-              <li><a class="dropdown-item" href="#">Daniele Savoia</a></li>
-              <li>
-                <a class="dropdown-item" href="#">Gary Smith Alvarez Venturo</a>
-              </li>
-            </ul>
           </li>
         </ul>
       </div>
